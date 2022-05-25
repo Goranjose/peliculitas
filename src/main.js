344f529d135c1c96566862ac845a67966dcb83d1
Orangel Gonzalez <goranjose16@gmail.com>
2022-05-24 20:58:40 -0400

    Add components and others files

--- src/main.js
+++ src/main.js
@@ -1,1 +1,70 @@
-alert("wtf es: " + API_KEY + "jum cual sera 🧐")
+// const axios = require('axios').default;
+// import axios from "axios";
+const api = axios.create({
+  baseURL: 'https://api.themoviedb.org/3',
+  "Content-Type": "application/json;chartset=utf-8",
+  params: {
+    api_key: API_KEY,
+  }
+})
+
+async function createMovie(movies, container) {
+  container.innerHTML = ''
+
+  movies.forEach(movie => {
+    // const trendingMoviesPreviewList = $("#trendingPreview .trendingPreview-movieList")
+    const movieContainer = document.createElement('div')
+    movieContainer.classList.add('movie-container')
+    const movieImg = document.createElement('img')
+    movieImg.classList.add('movie-img')
+    movieImg.alt = movie.title
+    movieImg.src = `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
+    movieContainer.appendChild(movieImg)
+    container.appendChild(movieContainer)
+  });
+}
+
+async function createCategory(categories, container) {
+  container.innerHTML = ''
+
+  categories.forEach(category => {
+    const categoryContainer = document.createElement('div')
+    categoryContainer.classList.add('category-container')
+    const categoryTitle = document.createElement('h3')
+    categoryTitle.classList.add('category-title')
+    categoryTitle.id = "id" + category.id
+    categoryTitle.addEventListener('click', () => {
+      location.hash = `#category=${category.id}-${category.name}`
+    })
+    const categoryTitleText = document.createTextNode(category.name)
+    // const categoryTitleText = document.textContent(category.name)
+    categoryTitle.appendChild(categoryTitleText)
+    categoryContainer.appendChild(categoryTitle)
+    container.appendChild(categoryContainer)
+  });
+}
+
+async function getTrendingMoviesPreview() {
+  const { data } = await api("/trending/movie/day")
+  const movies = data.results
+
+  createMovie(movies, trendingMoviesPreviewList)
+}
+
+async function getCategegoriesPreview() {
+  const { data} = await api(`/genre/movie/list`)
+  const categories = data.genres
+
+  createCategory(categories, categoriesPreviewList)
+}
+
+async function getMoviesByCategory(id) {
+  const { data } = await api("/discover/movie", {
+    params: {
+      with_genres: id,
+    },
+  })
+  const movies = data.results
+
+  createMovie(movies, genericSection)
+}
